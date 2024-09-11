d2f7674d954098d312f55c840004b63ccd9d31a0
Edjek <edjek@hotmail.fr>
2024-09-11 15:54:28 +0200

    initialisation

---
+++ 01-database.sh
@@ -0,0 +1,23 @@
+# ? Voici quelques commandes de base pour interagir avec une base de données MySQL :
+
+# Version de MySql :
+    mysql --version
+
+# Connexion à MySql:
+    mysql -u nom_utilisateur -p
+
+# Déconnexion de MySql :
+     quit
+     exit
+
+# Affichage des bases de données existantes :
+    SHOW DATABASES;
+
+# Création d'une base de données :
+     CREATE DATABASE nom_de_la_base;
+
+# Selectionner une base de données :
+    USE nom_de_la_base;
+
+# Supprimer une base de données :
+    DROP DATABASE nom_de_la_base;
---
+++ 02-tables.sh
@@ -0,0 +1,46 @@
+# ? LDD : Langage de Définition de Données (DDL : Data Definition Language)
+# ? Le langage de définition de données (DDL) est utilisé pour définir la structure de la base de données.
+
+# Voici quelques-unes des principales commandes pour créer et modifier une base de données dans MySQL :
+
+# Affichage des tables :
+   SHOW TABLES;
+
+CREATE TABLE user (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255),
+    age INT DEFAULT 0,
+    email VARCHAR(255) UNIQUE
+);
+
+# ! les noms des colonnes en anglais
+creer une table formation
+    identifiant
+    intitule # * unique et non null
+    description # * beaucoup de texte
+    duree # *superieur à 7 jours => check
+
+# Affichage de la structure d'une table :
+   SHOW CREATE TABLE nom_table;
+
+# Affichage des colonnes d'une table :
+   DESCRIBE nom_table;
+
+# Suppression d'une table :
+   DROP TABLE nom_table;
+
+# Renommer une table :
+   RENAME TABLE ancien_nom TO nouveau_nom;
+
+# Ajouter une colonne :
+   ALTER TABLE nom_table ADD nom_colonne type_de_donnees;
+
+# Modifier le type de données d'une colonne :
+   ALTER TABLE nom_table MODIFY colonne nouveau_type_de_donnees;
+
+# Modifier le nom d'une colonne :
+   ALTER TABLE nom_table CHANGE COLUMN ancien_nom nouveau_nom type_de_donnees;
+
+# Supprimer une colonne :
+   ALTER TABLE nom_table DROP COLUMN nom_colonne;
