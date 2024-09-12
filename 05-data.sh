40f47b136367c2102c9dd7c5912a54c0a7039667
Edjek <edjek@hotmail.fr>
2024-09-12 16:30:30 +0200

    day 2

--- 05-data.sh
+++ 05-data.sh
@@ -24,11 +24,35 @@
 
 
 # Je veux recuperer tous les livre paruent en 1960, 1970, 1987
+SELECT * FROM book WHERE year IN (1960, 1970, 1987);
 
 # Je veux recuperer tous les livres qui contiennent PAS la lettre a dans leur titre
+SELECT * FROM book WHERE title NOT LIKE '%a%';
+
 # Je veux recuperer tous les livres qui commencent par la lettre a dans leur titre
+
 # Je veux recuperer tous les livres triés par ordre decroissant de date de parution
+SELECT *
+FROM book
+WHERE title
+LIKE 't%'
+AND year> 1930
+AND id>=3
+ORDER BY year DESC
+;
+
 # Je veux recuperer tous les 3 premiers livres
+SELECT *
+FROM book
+LIMIT 3;
+
 # Je veux recuperer tous les livre qui ne sont PAS paruent en 1983
+SELECT *
+FROM book
+WHERE year != 1983;
+
 # Je veux recuperer tous les livre qui ne sont PAS paruent en 1960, 1970, 1987
 
+SELECT *
+FROM book
+WHERE year NOT IN (1960, 1987, 1878);
